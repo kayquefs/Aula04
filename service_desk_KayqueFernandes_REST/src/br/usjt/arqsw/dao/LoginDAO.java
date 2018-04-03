0d0dc2964de425150ffb42c719e03a1e6c6328ef
arqdsis <arqdsis@moclii0315.animaedu.intranet>
2018-04-03 20:38:45 -0300

    Aula04

---
+++ service_desk_KayqueFernandes_REST/src/br/usjt/arqsw/dao/LoginDAO.java
@@ -0,0 +1,24 @@
+package br.usjt.arqsw.dao;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import br.usjt.arqsw.entity.Login;
+
+@Repository
+public class LoginDAO {
+	@PersistenceContext
+	EntityManager manager;
+	
+		
+}
---
+++ service_desk_KayqueFernandes_REST/src/br/usjt/arqsw/service/LoginService.java
@@ -0,0 +1,26 @@
+package br.usjt.arqsw.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.usjt.arqsw.dao.LoginDAO;
+
+/**
+ * 
+ * @author Kayque Fernandes dos Santos 816121871
+ *
+ */
+
+
+@Service
+public class LoginService {
+	private LoginDAO dao;
+	
+	@Autowired
+	public LoginService(LoginDAO dao){
+		this.dao = dao;
+	}
+
+}
+
+
